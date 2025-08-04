9f5a5738ba5ac1aac4d03197249624daf8155046
LeonidUIN <leoniduin@gmail.com>
2025-08-04 03:48:20 +0300

    Подключил Тайлвинд по CDN

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Подключаем Tailwind через CDN */}
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >

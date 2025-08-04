36375713ffa554b57d287a631daec9c18e952017
LeonidUIN <leoniduin@gmail.com>
2025-08-04 12:30:17 +0300

    fix: remove commented <script> to satisfy Next.js lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Подключаем Tailwind через CDN */}
-        {/* <script src="https://cdn.tailwindcss.com"></script> */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
